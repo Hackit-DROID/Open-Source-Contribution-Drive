8985794776fa205ade79bb8092440163d8ee0d6b
SALONI <taronesaloni@gmail.com>
2026-09-23 21:29:49 +0530

    refactor: move hardcoded grade cutoffs, branches and rounding into constants (CR-1080)

--- Student_Information_Management_System_2/mis_project_marks/students/tests.py
+++ Student_Information_Management_System_2/mis_project_marks/students/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .views import get_grade
+
+
+class GradeTests(TestCase):
+    def test_grade_boundaries(self):
+        self.assertEqual(get_grade(95), 'A+')
+        self.assertEqual(get_grade(90), 'A+')
+        self.assertEqual(get_grade(89.99), 'A')
+        self.assertEqual(get_grade(80), 'A')
+        self.assertEqual(get_grade(70), 'B')
+        self.assertEqual(get_grade(60), 'C')
+        self.assertEqual(get_grade(59.99), 'F')
+        self.assertEqual(get_grade(0), 'F')
